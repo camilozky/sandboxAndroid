47694c8c493b3fb5d506c5b350c48fab3c41a314
Your Name <milo@Milos-MBP.attlocal.net>
2022-08-15 08:36:52 -0400

    remember

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,19 +3,11 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,31 +20,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val dog = Dog()
-                    val cat = Cat()
-                    val myAnimals: MutableList<String> = mutableListOf()
-                    val myAnimalsObject: MutableList<Animal> = mutableListOf()
-                    myAnimals.add(dog.makeSound())
-                    myAnimals.add(cat.makeSound())
-                    myAnimalsObject.add(cat)
-                    myAnimalsObject.add(dog)
-                    LazyColumn {
-                        items(myAnimals) {
-                            Row(
-                                Modifier.padding(8.dp),
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Text(text = it, fontSize = 12.sp)
-                            }
-                        }
-                        item {
-                            Button(
-                                modifier = Modifier.padding(8.dp),
-                                onClick = {  }) {
-                                Text(text = "Send message")
-                            }
-                        }
+                    Column() {
+                        Navigation()
+//                        HelLoContent()
                     }
+//                    val dog = Dog()
+//                    val cat = Cat()
+//                    val myAnimals: MutableList<String> = mutableListOf()
+//                    val myAnimalsObject: MutableList<Animal> = mutableListOf()
+//                    myAnimals.add(dog.makeSound())
+//                    myAnimals.add(cat.makeSound())
+//                    myAnimalsObject.add(cat)
+//                    myAnimalsObject.add(dog)
+//                    LazyColumn {
+//                        items(myAnimals) {
+//                            Row(
+//                                Modifier.padding(8.dp),
+//                                verticalAlignment = Alignment.CenterVertically
+//                            ) {
+//                                Text(text = it, fontSize = 12.sp)
+//                            }
+//                        }
+//                        item {
+//                            Button(
+//                                modifier = Modifier.padding(8.dp),
+//                                onClick = {  }) {
+//                                Text(text = "Send message")
+//                            }
+//                        }
+//                    }
                 }
             }
         }
--- app/src/main/java/com/example/myapplication/Navigation.kt
+++ app/src/main/java/com/example/myapplication/Navigation.kt
@@ -1,4 +1,116 @@
 package com.example.myapplication
 
-class Navigation {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route) {
+            MainScreen(navController = navController)
+        }
+        composable(
+            route = Screen.DetailScreen.route + "/{name}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+                    defaultValue = "Milo"
+                    nullable = true
+                }
+            )
+        )
+        {
+            DetailScreen(it.arguments?.getString("name"))
+        }
+    }
+
 }
+
+
+@Composable
+fun MainScreen(navController: NavController) {
+    var text by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        if (text.isNotEmpty())
+            Text(
+                text = "Hello, $text!",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.h5
+            )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = text,
+            onValueChange = { text = it },
+            label = {
+                Text("Name")
+            })
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            modifier = Modifier.align(Alignment.End),
+            onClick = {
+                navController.navigate(Screen.DetailScreen.withArgs(text))
+            }) {
+            Text(text = "to DetailScreen")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(name: String?) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    )
+    {
+        Text(text = "Hello, $name")
+    }
+}
+//
+//@Composable
+//fun HelLoContent() {
+//    Column(modifier = Modifier.padding(16.dp)) {
+//        var name by remember { mutableStateOf("") }
+//        Text(
+//            text = "Hello, $name",
+//            modifier = Modifier.padding(bottom = 8.dp),
+//            style = MaterialTheme.typography.h5
+//        )
+//        OutlinedTextField(
+//            value = name,
+//            onValueChange = { name = it },
+//            label = {
+//                Text("Name")
+//            })
+//    }
+//}
+
